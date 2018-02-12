ea9d96c309b996fddc8da2348f3ce7a1e7e027c8
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-02-12 13:52:19 -0200

    more union work

--- lang/union.c
+++ lang/union.c
@@ -10,5 +10,15 @@ union Data {
 int main( ) {
    union Data data;
    printf( "Memory size occupied by data : %lu\n", sizeof(data));
+
+   data.i = 10;
+   printf( "data.i : %d\n", data.i);
+
+   data.f = 220.5;
+   printf( "data.f : %f\n", data.f);
+
+   strcpy( data.str, "C Programming");
+   printf( "data.str : %s\n", data.str);
+
    return 0;
 }
