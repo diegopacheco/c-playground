25900bf363aefd0575e3476589114593f2b8058c
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-02-12 19:33:00 -0200

    added null in c'

---
+++ lang/null.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int *ptr;	/*integer pointer declaration*/
+	ptr=NULL;	/*Assigning the NULL to pointer*/
+
+	/*printing the value of NULL Macro*/
+	printf("Value of NULL Macro: %p\n",NULL);
+
+	/*printing the value of NULL Pointer*/
+	printf("Value of NULL pointer: %p\n",ptr);
+
+	return 0;
+}
