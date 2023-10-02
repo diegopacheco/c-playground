78cbfe71c16ac92652a4beb2df94a3fe1efe6250
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-02 02:37:15 -0700

    c

--- strings/build.sh
+++ strings/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+makdir target
 gcc main.c -o target/main
---
+++ strings/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+
+   char a[5] = {'H', 'e', 'l', 'l', 'o',};
+   printf("String: %s\n", a );
+   return 0;
+}
