033746e3f099fb5ba0e1cec6a6b2852d3e09156c
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-02 17:20:39 -0700

    pointers

--- pointers/main.c
+++ pointers/main.c
@@ -2,6 +2,7 @@
 
 void main(){
     values_pointers();
+    pointer_of_pointers();
 }
 
 void values_pointers(){
@@ -11,4 +12,13 @@ void values_pointers(){
     printf("value = %d \n",*pointer); 
     printf("ref   = %lu \n",&pointer);
     printf("ref   = %lu \n",&counter);
+}
+
+void pointer_of_pointers(){
+    int counter = 42;
+    int *pointer = &counter;
+    int **pp = &pointer;
+
+    printf("pp value = %d \n",**pp); 
+    printf("pp ref   = %lu \n",&pp);
 }
