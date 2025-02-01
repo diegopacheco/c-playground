e502b42cd011e7b4e283db0c662b3ae3eef7b41f
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-31 23:20:49 -0800

    c hex

--- hex/main.c
+++ hex/main.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void main(){
-  printf("Hello");
+void string_to_hex(const char *str) {
+    while (*str) {
+        printf("%02x", (unsigned char)*str);
+        str++;
+    }
+    printf("\n");
+}
+
+void hex_to_string(const char *hex) {
+    while (*hex && *(hex + 1)) {
+        char byte[3] = { *hex, *(hex + 1), '\0' };
+        printf("%c", (char)strtol(byte, NULL, 16));
+        hex += 2;
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s --hex <string> | --text <hex>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "--hex") == 0) {
+        string_to_hex(argv[2]);
+    } else if (strcmp(argv[1], "--text") == 0) {
+        hex_to_string(argv[2]);
+    } else {
+        printf("Invalid option. Use --hex or --text.\n");
+        return 1;
+    }
+
+    return 0;
 }
