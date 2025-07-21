cbb3898dff012f9c39555ea5ce7f64de832c0e5e
diegopacheco <diego.pacheco.it@gmail.com>
2025-07-20 17:24:02 -0700

    added C lang multiline str cocnat

---
+++ string-literal-concat/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(){
+  const char *multilineString = "This is a very long string "
+                       "that spans multiple lines "
+                       "for better readability.";
+  printf("%s\n", multilineString);
+}
